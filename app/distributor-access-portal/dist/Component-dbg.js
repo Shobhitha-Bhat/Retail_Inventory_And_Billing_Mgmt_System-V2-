4bc0ab9fa6b6d9d7e9d206378b1d3a7063cadb03
Shobhitha Bhat <shobhitha.cs22@sahyadri.edu.in>
2026-04-06 06:11:52 +0000

    1st deployed version

---
+++ app/distributor-access-portal/dist/Component-dbg.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("distributoraccessportal.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
